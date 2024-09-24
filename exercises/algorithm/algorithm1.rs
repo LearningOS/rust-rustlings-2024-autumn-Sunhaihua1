2283bfe441b7c1f58522f9648c43e50bf84d1192
Sunhaihua1 <2660245132@qq.com>
2024-09-24 14:28:07 +0800

    clippy2_fix

--- exercises/algorithm/algorithm1.rs
+++ exercises/algorithm/algorithm1.rs
@@ -71,16 +71,8 @@ impl<T> LinkedList<T> {
     }
 	pub fn merge(list_a:LinkedList<T>,list_b:LinkedList<T>) -> Self
 	{
-        unsafe{
-            *list_a.end.next = list_b.start;
-        }
-		//TODO
-		Self {
-            length: list_a.length + list_b.length,
-            start: list_a.start,
-            end: list_b.end,
-        }
-	}
+        
+    }
 }
 
 impl<T> Display for LinkedList<T>
